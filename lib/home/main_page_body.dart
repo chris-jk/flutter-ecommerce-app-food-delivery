2a353a8d8fc0a8761bcaa5e7f9554153c3dc27a0
christopher james <kushyapp916@gmail.com>
2022-07-30 12:35:14 -0700

    finshed scaling of banner

--- lib/home/main_page_body.dart
+++ lib/home/main_page_body.dart
@@ -14,7 +14,8 @@ class MainPageBody extends StatefulWidget {
 class _MainPageBodyState extends State<MainPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue = 0.0;
-  final double _scaleFactor = 0.8;
+  double _scaleFactor = 0.8;
+  double _height = 220;
 
   @override
   void initState() {
@@ -50,13 +51,28 @@ class _MainPageBodyState extends State<MainPageBody> {
 
     if (index == _currPageValue.floor()) {
       var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
     } else if (index == _currPageValue.floor() + 1) {
       var currScale =
           _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1);
-    }
+      var currTrans = _height * (1 - currScale) / 2;
 
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index - 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - currScale) / 2, 1);
+    }
     return Transform(
       transform: matrix,
       child: Stack(

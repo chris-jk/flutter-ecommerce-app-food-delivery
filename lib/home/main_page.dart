c4e8f5d27255f040a33eeba875bb4f567c48e2f8
christopher james <kushyapp916@gmail.com>
2022-07-31 16:18:34 -0700

    made padding /bigtext/icon dynamic

--- lib/home/main_page.dart
+++ lib/home/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/home/main_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
@@ -15,46 +16,53 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          child: Container(
-            margin: EdgeInsets.only(top: 45, bottom: 15),
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    BigText(text: "US", color: AppColors.mainColor),
-                    Row(
-                      children: [
-                        SmallText(text: "City", color: Colors.black54),
-                        Icon(Icons.arrow_drop_down_rounded)
-                      ],
-                    )
-                  ],
-                ),
-                Center(
-                  child: Container(
-                    width: 45,
-                    height: 45,
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: AppColors.mainColor,
-                    ),
+      body: Column(
+        children: [
+          // showing header
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(
+                  top: Dimensions.height45, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(
+                  left: Dimensions.width20, right: Dimensions.width20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "US", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: "City", color: Colors.black54),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
                   ),
-                )
-              ],
+                  Center(
+                    child: Container(
+                      width: Dimensions.width45,
+                      height: Dimensions.height45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: Dimensions.iconSize24,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radius15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        MainPageBody(),
-      ],
-    ));
+          // showing the body
+          MainPageBody(),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/icon_and_text.dart
+++ lib/widgets/icon_and_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
 class IconAndText extends StatelessWidget {
@@ -20,8 +21,9 @@ class IconAndText extends StatelessWidget {
         Icon(
           icon,
           color: iconColor,
+          size: Dimensions.iconSize24,
         ),
-        SizedBox(width: 5),
+        SizedBox(width: Dimensions.height5),
         SmallText(text: text)
       ],
     );
